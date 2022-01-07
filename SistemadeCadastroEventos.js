de7daf6c2bbbfa07cdad79d21e4825ae57bc2a38
Alexandrecrs77 <94266292+Alexandrecrs77@users.noreply.github.com>
2022-01-07 19:23:12 -0300

    Add files via upload

--- SistemadeCadastroEventos.js
+++ SistemadeCadastroEventos.js
@@ -2,26 +2,26 @@
 dataEvento = 10/12/2021
 let = dataInscricao
 if(dataInscricao < dataEvento) {
-    console.log("Cadastrar Participantes")
+  console.log("Cadastrar Participantes")
 } else {
-    console.log("Cadastro não permitido")
+  console.log("Cadastro não permitido")
 }
 
 //Análise da idade do participante 
-//Idade = DataIncscricao - DataNascimento
-let idade = DataIncscricao - DataNascimento
+//Idade = 18
+let idade = 18
 if(idade >= 18) {
-    console.log("Permitir cadastro")
+   console.log("Permitir cadastro")
 } else { 
     console.log("Alerta: Cadastro não permitido pela idade")
 }
 
 //Lista de palestrantes
-const listadePalestrantes = [0,1,2,3,4]
+//const listadePalestrantes = [0,1,2,3,4]
 
-// Análise da quantidade de participantes
-var listadeParticipantes = [0,1,2,3,4,...99] 
-if(listadeParticipantes <= 100 ) {
+// Analise da quantidade de participantes
+var listadeParticipantes = ["Alexandre", "Jéssica", "Raoni"] 
+if(listadeParticipantes.length <= 100) {
     console.log("Permitir cadastro")
 } else {
     console.log("Alerta: Cadastro não permitido Limite excedido")
